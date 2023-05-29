6727b8c770f8bc17019a5b01a84a3b903f218872
codemask <mattode@outlook.com>
2023-05-29 15:13:25 +0100

    response data schema

--- app/schema.py
+++ app/schema.py
@@ -4,3 +4,13 @@ class PaybackSchema(BaseModel):
     annual_energy_consumption: int = Field(..., description="The annual energy consumption in kWh")
     installation_cost: int = Field(..., description="The cost of the solar panel installation")
     installation_wp: int = Field(..., description="The watt peak of the installed solar panel")
+
+
+class OptimalSolutionData(BaseModel):
+    shortest_payback_time: float
+    solar_panels_in_Wp: int
+
+
+class PaybackResponse(BaseModel):
+    payback_time_in_years: float
+    optimal_solution: OptimalSolutionData
